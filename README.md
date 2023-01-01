e8236f9e672b6a06ef5ada6d5ca1279fc3437816
EvgenySushko <evgeny.sushko@gmail.com>
2023-01-01 07:14:08 +0100

    JAN.03

--- README.md
+++ README.md
@@ -13,3 +13,9 @@ Daily creative coding sketches for [Genuary 2023](https://genuary.art/)
 > Made in 10 minutes
 
 <img src="./output/JAN02_1.png" width="200"/> <img src="./output/JAN02_2.png" width="200"/> <img src="./output/JAN02_3.png" width="200"/> <img src="./output/JAN02_4.png" width="200"/> 
+
+## JAN.03
+
+> Glitch Art
+
+<img src="./output/JAN03_1.png" width="200"/> <img src="./output/JAN03_2.png" width="200"/> <img src="./output/JAN03_3.png" width="200"/> <img src="./output/JAN03_4.png" width="200"/> 
---
+++ sketches/JAN03/sketch.js
@@ -0,0 +1,66 @@
+/*
+Genuary 2023
+JAN.03 "Glitch Art"
+
+By Evgeny Sushko
+https://github.com/evgenysushko
+*/
+
+"use strict";
+
+let w = 800;
+let img;
+let colors = [];
+
+function preload() {
+  img = loadImage("sketches/JAN03/data/klimt.jpeg");
+}
+
+function setup() {
+  // pixelDensity(1);
+  createCanvas(w, w);
+  noStroke();
+  ellipseMode(CORNER);
+}
+
+function draw() {
+  noLoop();
+  background(255);
+
+  let tileCount = 400;
+  let rectSize = floor(width / tileCount);
+
+  img.loadPixels();
+  colors = [];
+
+  for (let gridY = 0; gridY < tileCount; gridY++) {
+    for (let gridX = 0; gridX < tileCount; gridX++) {
+      let px = int(gridX * rectSize);
+      let py = int(gridY * rectSize);
+      let i = (py * img.width + px) * 4;
+      let c = color(
+        img.pixels[i],
+        img.pixels[i + 1],
+        img.pixels[i + 2],
+        img.pixels[i + 3]
+      );
+      colors.push(c);
+    }
+  }
+
+  let i = 0;
+  for (let gridY = 0; gridY < tileCount; gridY++) {
+    for (let gridX = 0; gridX < tileCount; gridX++) {
+      fill(colors[i]);
+      push();
+      rotate(random(0.05));
+      square(gridX * rectSize, gridY * rectSize, rectSize);
+      pop();
+      i++;
+    }
+  }
+}
+
+function keyPressed() {
+  if (key == "s" || key == "S") saveCanvas("output", "png");
+}
